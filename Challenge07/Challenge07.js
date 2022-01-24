e770706ec09836ca7cd721d3c7bb19f37744e893
rehamjdetawi <rorojdetawe@gmail.com>
2022-01-24 22:41:39 +0200

    solve problem solving2

---
+++ Challenge07/Challenge07.js
@@ -0,0 +1,146 @@
+
+   
+"use strict";
+
+// 1) ---------------------
+//
+//  using the (objLat) function; return the formatted text based on the provided object as the example:
+//  let obj = {
+//      firstName: 'Jaber',
+//      lastName: 'Saleh',
+//      age: 67,
+//      hobby: 'Gaming and Sleeping'
+//  }
+//   ===>
+//   'my name is Jaber Saleh I am 67 YO, and I love Gaming and Sleeping.'
+
+//  Note that:
+//  1- The text template is 'my name is ** ** I am ** YO, and I love **.'
+//  2- The first letters of the firstName and lastName should be capital letter
+
+// ------------------------
+
+const objLat = (obj) => {
+    // write your code here
+    let myInfo=`my name is ${obj.firstName.charAt(0).toUpperCase()+obj.firstName.slice(1)} ${obj.lastName.charAt(0).toUpperCase()+obj.lastName.slice(1)} I am ${obj.age} YO, and I love ${obj.hobby}.`;
+    return myInfo;
+};
+
+// 2) ---------------------
+//
+//  Rekey is receiving info about applicants for his startup company (as array of objects), containing first name,
+//  last name, age and technology they know.
+//  Rekey only cares about the full name and the technology if the applicant have more than one year of Experience
+//
+//  Reformat the array of objects that you are receiving by returning a new array of objects that contain only
+//  fullName and tech if the applicant has more than one year of Experience
+
+// EX:
+//let cvs = [
+//     {
+//         firstName: "Jason",
+//         LastName: "James",
+//         yearsOfExperience: 20,
+//         tech: "JS"
+//     },
+//     {
+//         firstName: "Shira",
+//         LastName: null,
+//         yearsOfExperience: 5,
+//         tech: ".Net"
+//     },
+//     {
+//         firstName: "Dmitri",
+//         LastName: "Akkerman",
+//         yearsOfExperience: 1,
+//         tech: "Python"
+//     },
+//     {
+//         firstName: "Isabella",
+//         LastName: null,
+//         yearsOfExperience: 7,
+//         tech: "Java"
+//     }
+// ]
+//
+//   ===>
+
+//
+// [
+//     {
+//         fullName: "Jason James",
+//         tech: "JS"
+//     },
+//     {
+//         fullName: "Shira",
+//         tech: ".Net"
+//     },
+//     {
+//         fullName: "Isabella",
+//         tech: "Java"
+//     }
+// ] 
+
+//  Note that:
+//  1- Full name is first name + last name
+//  2- If one of the names is null dont add it to the full name
+
+// ------------------------
+   
+    
+const cvFormatter = (arr) => {
+    let newArr=[];
+    for(var x=0;x<arr.length;x++){
+    if(this.tech>1)
+    {newArr[x]=[this.firstName,this.lastName,this.tech];
+
+    }
+
+}
+return newArr;    // write your code here
+};
+
+// 3) ---------------------
+//
+//  Rekey wants to get statistic about the applicants
+//  using the array of object you will be getting, return an object that has the following properties
+//
+// let result = {
+//     python_Devs: 0,
+//     javaScript_Devs: 0,
+//     dotNet_Devs: 0,
+//     java_Devs: 0,
+//     totalApplicants: 0,
+//     rejectedApplicants: 0,
+// }
+//
+// and fill it up based on the results
+
+//  Note that:
+//  1- rejectedApplicants are applications that has both the names empty or null and whoever have less than one year of Experience
+
+// ------------------------
+const applicationsStatics = () => {
+    // write your code here
+};
+
+// 4) ---------------------
+//
+//  A Certain School principal wants to calculate the average score of each class in each grade in the school
+//  so he requested a programmer to solve this issue for him instead of doing it again every time
+//
+//  given the school data, calculate the average score of the students of each class in each grade
+//  then change the avg value for the class
+//  EX:
+
+
+//  Note that:
+//  1- This is not the exact data you will be getting every time and the solution should be dynamic
+//  2- You need to round the average to the nearest lower number 
+
+const classesAvg = () => {
+    // write your code here
+    
+};
+
+module.exports = { objLat, cvFormatter, applicationsStatics, classesAvg };
